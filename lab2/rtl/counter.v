3d6a3a62dd138c4cfdefc43ef20999f9d37e4848
unclefalkon <62942388+unclefalkon@users.noreply.github.com>
2020-05-11 17:36:15 +0300

    Edited codestyle in counter.v

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
+
 module counter(
-    input         clk_i,
-    input  [9:0]   sw_i,
-    input  [1:0]  key_i,
-    output [9:0] ledr_o,
-    output [6:0] hex1_o,
-    output [6:0] hex0_o);
+    input         clk100_i,
+    input  [9:0]      sw_i,
+    input  [1:0]     key_i,
+    output [9:0]    ledr_o,
+    output [6:0]    hex1_o,
+    output [6:0]    hex0_o);
 
 reg [9:0] register;
 reg [7:0] counter;
@@ -13,13 +14,13 @@ reg [7:0] counter;
 wire bt_down;
 
 pinched btdown(
-  .bt_i  ( !key_i[0] ),
-  .rst_i  ( key_i[1] ),
-  .clk_i     ( clk_i ),
-  .btnd_o  ( bt_down )
+  .bt_i   (    !key_i[0] ),
+  .rst_i  (     key_i[1] ),
+  .clk_i  (     clk100_i ),
+  .btnd_o (      bt_down )
 );
 
-always @ ( posedge clk_i or negedge key_i[1]) begin
+always @ ( posedge clk100_i or negedge key_i[1]) begin
   if ( !key_i[1] ) begin
    register <= 10'b0;
    counter  <=  8'd0;
@@ -34,13 +35,13 @@ end
 assign ledr_o = register;
 
 dectohex dec0
-( .in ( counter[7:4] ),
-  .out( hex0_o       )
+( .in  ( counter[7:4] ),
+  .out ( hex0_o       )
 );
   
 dectohex dec1
-( .in ( counter[3:0] ),
-  .out( hex1_o       )
-);
+( .in  ( counter[3:0] ),
+  .out ( hex1_o       )
+);  
 
 endmodule
