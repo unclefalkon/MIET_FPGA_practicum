c19052c9ff97596ddd00e780bbe02883fba179ba
unclefalkon <62942388+unclefalkon@users.noreply.github.com>
2020-05-13 16:50:05 +0400

    Updated counter.v for individual task.
    Добавлено еще два семисегментных счетчика dec2 и dec3, счетчик counter теперь 16-разрядный, теперь обработка регистра и счетчика происходит в двух разных блоках, из-за разницы условий.

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -3,45 +3,61 @@
 module counter(
     input         clk100_i,
     input  [9:0]      sw_i,
-    input  [1:0]     key_i,
+    input  [2:0]     key_i,
     output [9:0]    ledr_o,
+    output [6:0]    hex0_o,
     output [6:0]    hex1_o,
-    output [6:0]    hex0_o);
+    output [6:0]    hex2_o,
+    output [6:0]    hex3_o);
 
 reg [9:0] register;
-reg [7:0] counter;
+reg [15:0] counter;
 
 wire bt_down;
 
 pinched btdown(
-  .bt_i   (    !key_i[0] ),
-  .rst_i  (     key_i[1] ),
-  .clk_i  (     clk100_i ),
-  .btnd_o (      bt_down )
+  .bt_i   (    !key_i[0] & !key_i[2] ),
+  .rst_i  (    key_i[1]              ),
+  .clk_i  (    clk100_i              ),
+  .btnd_o (    bt_down               )
 );
 
-always @ ( posedge clk100_i or negedge key_i[1]) begin
-  if ( !key_i[1] ) begin
-   register <= 10'b0;
-   counter  <=  8'd0;
+always @ ( posedge clk100_i or negedge key_i[1] )
+ begin
+   if ( !key_i[1] )
+     register <= 10'b0;
+  else if ( bt_down )
+     register <= sw_i;
+ end 
+ 
+always @ ( posedge clk100_i or negedge key_i[1] )
+  begin
+    if ( !key_i[1] )
+      counter <= 16'd0;
+    else if ( bt_down && ( sw_i > 10'd20 ) )
+      counter <= counter + 1;
   end
-  else
-    if ( bt_down ) begin
-      register <=        sw_i;
-      counter  <= counter + 1;
-    end
-end 
 
 assign ledr_o = register;
 
 dectohex dec0
-( .in  ( counter[7:4] ),
+( .in  ( counter[3:0] ),
   .out ( hex0_o       )
 );
   
 dectohex dec1
-( .in  ( counter[3:0] ),
+( .in  ( counter[7:4] ),
   .out ( hex1_o       )
-);  
+);
+
+dectohex dec2
+( .in  ( counter[11:8] ),
+  .out ( hex2_o        )
+);
+
+dectohex dec3
+( .in  ( counter[15:12] ),
+  .out ( hex3_o         )
+);
 
 endmodule
