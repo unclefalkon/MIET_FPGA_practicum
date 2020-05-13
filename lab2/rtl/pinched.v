d25d9adc5d76c46314174446402bdbb392082f74
unclefalkon <62942388+unclefalkon@users.noreply.github.com>
2020-05-13 16:51:27 +0400

    Upted pinched.v module for individual task.
    Теперь в этот модуль включена обработка дребезга и для кнопки key_i[2].

--- lab2/rtl/pinched.v
+++ lab2/rtl/pinched.v
@@ -6,18 +6,19 @@ module pinched(
   input   bt_i,
   output btnd_o);
 
-reg [1:0] synch;
+reg [2:0] synch;
 
 always @ ( posedge clk_i or negedge rst_i ) begin
   if ( !rst_i )
-    synch <= 2'b0;
+    synch <= 3'b0;
   else
     begin
       synch[0] <=     bt_i;
       synch[1] <= synch[0];
+      synch[2] <= synch[1];
     end
 end
 
-assign btnd_o = synch[0] & ~synch[1];
+assign btnd_o = synch[1] & ~synch[2];
 
 endmodule
